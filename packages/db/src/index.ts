0c8c0b7ba58fc85ab5f364182a838d81849b6285
Ast <varistyshark@gmail.com>
2025-10-15 11:39:38 -0400

    Export tables

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
 export const db = drizzle(process.env.DATABASE_URL || "");
+
+// Export all tables
+export * from "./schema/auth";
+export * from "./schema/todo";
+export * from "./schema/referral";
