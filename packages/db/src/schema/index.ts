a9f0cc72c25b3f18909439c652152390da933155
Ast <varistyshark@gmail.com>
2025-10-19 22:20:57 -0400

    remove todo from export

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,5 +1,4 @@
 // Export all schema tables for Drizzle query API
 export * from "./auth";
-export * from "./todo";
 export * from "./referral";
 // export * from "./views"; // Commented out - will be handled separately
